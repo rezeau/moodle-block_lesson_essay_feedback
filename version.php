58274d4aaf05a2238d04203c0011df7d0f5c1fb1
rezeau <joseph@rezeau.org>
2020-11-26 17:55:51 +0100

    bumped version for moodle 3.10 (with fix)
    Signed-off-by: rezeau <joseph@rezeau.org>

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019112100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2020112600;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014050800;        // Requires this Moodle version
 $plugin->component = 'block_lesson_essay_feedback';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.8.0 for moodle 3.8';
+$plugin->release = '3.10.0 for moodle 3.10';
